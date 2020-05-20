4e72950f75c844da6f6886f05e95802d36dae329
sukhd <centaurgolden@gmail.com>
2020-05-19 22:18:09 -0500

    connected to trends api

--- bot.js
+++ bot.js
@@ -2,4 +2,17 @@ const Twit = require("twit");
 const config = require("./config.js");
 
 let T = new Twit(config);
-console.log(T);
+
+var params = {
+  /*   q: "rainbow",
+  count: 2, */
+  id: 1,
+};
+T.get("trends/place", params, gotData);
+
+function gotData(err, data, response) {
+  let trends = data[0].trends;
+  let location = data[0].keys();
+  console.log(location);
+  trends.forEach((trend) => console.log(trend.name));
+}
