49791420e3c6d6cf628ba7196307fbeaeb1aece7
sukhd <centaurgolden@gmail.com>
2020-05-19 23:07:28 -0500

    trying to connect to woeid

--- bot.js
+++ bot.js
@@ -1,18 +1,19 @@
 const Twit = require("twit");
 const config = require("./config.js");
+const woeid = require("woeid");
+console.log(woeid.getWoeid("canada"));
 
 let T = new Twit(config);
 
 var params = {
-  /*   q: "rainbow",
-  count: 2, */
-  id: 1,
+  id: 2459115,
+  exclude: "hashtags",
 };
 T.get("trends/place", params, gotData);
 
 function gotData(err, data, response) {
   let trends = data[0].trends;
-  let location = data[0].keys();
-  console.log(location);
-  trends.forEach((trend) => console.log(trend.name));
+  let location = data[0].locations.name;
+  // console.log(location);
+  /// trends.forEach((trend) => console.log(trend.name));
 }
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: "BBkWNlIt5hj8WSxdiAfwqB7dz",
+  consumer_secret: "ufTaEQMeUDlLJqmsM6zybbax5ZhNW5FjXSKEr2OqNSIMcHbWen",
+  access_token: "3148800468-jgYFrkW2qVCN8kIMgkXowlpkMEyNXctKpeowdls",
+  access_token_secret: "lBW0bjHBB1d1myzatn2RiUzWwac3XEeiMraV0d4NDiRRA",
+};
